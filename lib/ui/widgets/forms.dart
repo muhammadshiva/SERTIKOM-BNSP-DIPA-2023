7dec42c89e21cb5332802f91920b20183d72e014
muhammadshiva <shivamuhammad07@gmail.com>
2023-09-30 10:48:45 +0700

    init project

---
+++ lib/ui/widgets/forms.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/theme.dart';
+
+class CustomFormField extends StatelessWidget {
+  const CustomFormField({
+    Key? key,
+    required this.title,
+    this.obscureText = false,
+    this.controller,
+    this.isShowTitle = true,
+    this.keyboardType,
+    this.onFieldSubmitted,
+  }) : super(key: key);
+
+  final String title;
+  final bool obscureText;
+  final TextEditingController? controller;
+  final bool isShowTitle;
+  final TextInputType? keyboardType;
+  final Function(String)? onFieldSubmitted;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (isShowTitle)
+          Text(
+            title,
+            style: blackTextStyle.copyWith(fontWeight: medium),
+          ),
+        if (isShowTitle) const SizedBox(height: 8),
+        TextFormField(
+          obscureText: obscureText,
+          controller: controller,
+          keyboardType: keyboardType,
+          decoration: InputDecoration(
+            hintText: !isShowTitle ? title : null,
+            hintStyle: greyTextStyle,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(14),
+            ),
+            contentPadding: const EdgeInsets.all(12),
+          ),
+          onFieldSubmitted: onFieldSubmitted,
+        ),
+      ],
+    );
+  }
+}
